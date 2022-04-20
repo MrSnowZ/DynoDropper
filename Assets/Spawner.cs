92fcb9c5cbb1669bc88d1c5f649e113b1582bf42
FlamingDiamond2K <Vince.Davis@govalor.com>
2022-04-20 09:50:13 -0600

    Spawner

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class WaveSpawner : MonoBehaviour{
 
+    public enum SpawnState  { SPAWNING, WAITING, COUNTING };
+
 
     [System.Serializable]
     Public class Wave
@@ -19,9 +21,69 @@ public class WaveSpawner : MonoBehaviour{
         public float timeBetweenwaves = 5f;
         private float waveCountdown = 3f;
 
-        void Start () {
+        private SpawnState state = SpawnState.COUNTING;
+
+        void Start () 
+        {   
+            waveCountdown = timeBetweenwaves;
+        }
+
+        void Update () 
+        {
+            if (state == SpawnState.WAITING) 
+            {
+                // Check if enemies are still alive
+            }
+
+            if (waveCountdown <= 0)
+            {
+                if (state != SpawnState.SPAWNING) 
+                {
+                    StartCoroutine ( SpawnWave ( waves[nextWave] ) );
+                } 
+            }
+            else 
+            {
+                waveCountdown -= Time.deltaTime;
+            }
+        } 
+
+        bool EnemyIsAlive() 
+        {
+            if (GameObject.FindGameObjectWithTag ("Enemy" == null)
+        }
+        return false;
+        {
 
         }
+
+        return true;
+        
+         {
+
+        }
+        
+        IEnumerator SpawnWave (Wave _wave) 
+        { 
+            state = SpawnState.SPAWNING;
+
+            for (int = 0; i < _wave.count; i++ )
+            {
+                SpawnEnemy (_wave.enemy);
+                yeild return new WaitForSeconds ( 1f/_wave.rate  );
+            }
+
+            state = SpawnState.WAITING;
+
+            yield break;
+        }
+
+        void SpawnEnemy (Transform _enemy) 
+        {
+            //spawn enemy
+            Debug.Log ("Spawing Enemy: " + _enemmy.name);
+        }
+        
     }
 
 
