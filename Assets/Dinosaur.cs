1bb3854d631585e408ae8117195492b719062ea6
Nick <62675916+MrSnowZ@users.noreply.github.com>
2022-05-10 14:33:04 -0600

    v0.11.3
    - Fix score board

--- Assets/Dinosaur.cs
+++ Assets/Dinosaur.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Dinosaur : MonoBehaviour {
-    //score = 0
+    public double score = 0;
     private void OnCollisionEnter2D(Collision2D other){
             Debug.Log("hit detected");
             Destroy(other.gameObject);
+            score += 0.5;
             //get game object by tag (scoreboard)
-            //get text
+            GameObject scoreBoard = GameObject.FindGameObjectWithTag("scoreboard");
+            scoreBoard.GetComponent<Text>().text = ""+(int)score;
             //set text to score +1
     }
 }
