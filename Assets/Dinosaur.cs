73154855c6e2a06ad086b16fa7deb572f7eb92f3
Nick <62675916+MrSnowZ@users.noreply.github.com>
2022-05-10 14:08:29 -0600

    v0.11.2
    - Log higgins notes

--- Assets/Dinosaur.cs
+++ Assets/Dinosaur.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Dinosaur : MonoBehaviour {
+    //score = 0
     private void OnCollisionEnter2D(Collision2D other){
             Debug.Log("hit detected");
             Destroy(other.gameObject);
+            //get game object by tag (scoreboard)
+            //get text
+            //set text to score +1
     }
 }
